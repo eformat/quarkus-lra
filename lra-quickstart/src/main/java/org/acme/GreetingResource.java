78210332e54dfdfbe26860c66411dd6fd2ca5208
Mike Hepburn <eformat@gmail.com>
2021-08-31 08:25:42 +1000

    🐻‍❄️ ice bear says better debug 🐻‍❄️

--- lra-quickstart/src/main/java/org/acme/GreetingResource.java
+++ lra-quickstart/src/main/java/org/acme/GreetingResource.java
@@ -1,19 +1,15 @@
 package org.acme;
 
-import org.eclipse.microprofile.lra.annotation.Compensate;
-import org.eclipse.microprofile.lra.annotation.Complete;
+import org.eclipse.microprofile.lra.annotation.*;
 import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.HeaderParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.Random;
 
 import static org.eclipse.microprofile.lra.annotation.ws.rs.LRA.LRA_HTTP_CONTEXT_HEADER;
 
@@ -31,6 +27,7 @@ public class GreetingResource {
     }
 
     // Step 2d: There must be a method to compensate for the action if it's cancelled
+    // does not end the LRA
     @PUT
     @Path("compensate")
     @Compensate
@@ -39,13 +36,21 @@ public class GreetingResource {
         return Response.ok(lraId.toASCIIString()).build();
     }
 
-    // Step 2e: An optional callback notifying that the LRA is closing
     @PUT
     @Path("tidyup")
-    @LRA(value = LRA.Type.MANDATORY)
+    @LRA(value = LRA.Type.MANDATORY,
+            cancelOn = {
+                    Response.Status.INTERNAL_SERVER_ERROR // cancel on a 500 code
+            },
+            cancelOnFamily = {
+                    Response.Status.Family.CLIENT_ERROR // cancel on any 4xx code
+            },
+            end = true) // end the LRA
     @Compensate
     public Response compensateWorkAndEnd(@HeaderParam(LRA_HTTP_CONTEXT_HEADER) URI lraId) {
         log.info("compensate and end {}", lraId);
+        if (new Random().nextBoolean())
+            return Response.serverError().build();
         return Response.ok(lraId.toASCIIString()).build();
     }
 
@@ -73,5 +78,28 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String end(@HeaderParam(LRA_HTTP_CONTEXT_HEADER) URI lraId) {
         return lraId.toASCIIString();
+    }
+
+    @PUT
+    @Path("/after")
+    @AfterLRA // method is called when the LRA associated with the method activityWithLRA finishes
+    public Response notifyLRAFinished(@HeaderParam(LRA.LRA_HTTP_ENDED_CONTEXT_HEADER) URI lraId,
+                                      LRAStatus status) {
+        switch (status) {
+            case Closed:
+                log.info("LRA closed and finished OK {}", lraId);
+                // the LRA was successfully closed
+                break;
+            default:
+                log.info("LRA finished {}", lraId);
+        }
+        return Response.ok(lraId.toASCIIString()).build();
+    }
+
+    @Forget
+    @Path("/forget")
+    @DELETE
+    public Response forget(@HeaderParam(LRA_HTTP_CONTEXT_HEADER) URI lraId) {
+        return Response.ok().build();
     }
 }
